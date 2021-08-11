1093bb58b3b4062a5c22622263f7a5e62c1bc857
Linda Amaya <linda.amaya.cetina@gmail.com>
2021-08-11 17:57:56 -0500

    Initial commit

---
+++ app/src/main/java/com/misactividades/view/ui/activity_splashscreen.kt
@@ -0,0 +1,12 @@
+package com.misactividades.view.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.misactividades.R
+
+class activity_splashscreen: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
